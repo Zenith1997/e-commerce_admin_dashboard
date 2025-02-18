82a0d7bfc608702f953d22a112e3fc94206f26eb
Zenith1997 <zenithanthony.official@gmail.com>
2025-02-19 02:10:11 +0530

    welcome meswsage modified

--- src/sections/overview/view/overview-analytics-view.tsx
+++ src/sections/overview/view/overview-analytics-view.tsx
@@ -20,7 +20,7 @@ export function OverviewAnalyticsView() {
   return (
     <DashboardContent maxWidth="xl">
       <Typography variant="h4" sx={{ mb: { xs: 3, md: 5 } }}>
-        Hi, Welcome back 👋
+        Hi, Welcome backsdasadsad 👋
       </Typography>
 
       <Grid container spacing={3}>
