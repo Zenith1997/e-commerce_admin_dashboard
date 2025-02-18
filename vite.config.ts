897f95a2ec708078118e76e4b2650b49444136ca
Zenith1997 <zenithanthony.official@gmail.com>
2025-02-19 01:28:39 +0530

    minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // ----------------------------------------------------------------------
 
-const PORT = 3039;
+const PORT = 8080;
 
 export default defineConfig({
   plugins: [
@@ -35,6 +35,6 @@ export default defineConfig({
       },
     ],
   },
-  server: { port: PORT, host: true },
+ 
   preview: { port: PORT, host: true },
 });
