73267a614da8790312ed66fde97b16265f817356
Zenith1997 <zenithanthony.official@gmail.com>
2025-02-19 01:39:49 +0530

    d

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,36 @@ import react from '@vitejs/plugin-react-swc';
 
 // ----------------------------------------------------------------------
 
+const PORT = 3039;
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:'/'
-})
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
+        dev: { logLevel: ['error'] },
+      },
+      overlay: {
+        position: 'tl',
+        initialIsOpen: false,
+      },
+    }),
+  ],
+    base:'/e-commerce_admin_dashboard/',
+  resolve: {
+    alias: [
+      {
+        find: /^~(.+)/,
+        replacement: path.join(process.cwd(), 'node_modules/$1'),
+      },
+      {
+        find: /^src(.+)/,
+        replacement: path.join(process.cwd(), 'src/$1'),
+      },
+    ],
+  },
+  server: { port: PORT, host: true },
+  preview: { port: PORT, host: true },
+});
