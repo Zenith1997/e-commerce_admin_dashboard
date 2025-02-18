606831bca2f1678a95625a6c8f1ea813802eb651
Zenith1997 <zenithanthony.official@gmail.com>
2025-02-19 01:36:35 +0530

    base changed

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/e-commerce_admin_dashboard/'
+  base:'/'
 })
